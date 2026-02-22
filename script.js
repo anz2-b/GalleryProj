0f40306b4366a230860ee40639ae27aadb5db245
anz2-b <bigvava.anzor@gmail.com>
2026-02-22 21:48:28 +0400

    completed quotes

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    const Profilebtn = document.getElementById("Profile");
-    const Gallerybtn = document.getElementById("Gallery");
-    const Timerbtn = document.getElementById("Timer");
-    const Thoughtsbtn = document.getElementById("Thoughts");
-    
     const music = document.getElementById("audioPlayer");
     const sword = document.getElementById("invertedspear");
     
@@ -13,15 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
             music.play();
         });
     }
-    
     const quote = document.getElementById("quote");
-
-    fetch(`https://api.quotable.io/random?content`)
+    function newQuote(){
+    fetch(`https://api.quotable.io/random`)
     .then(response => response.json())
-    .then(data => {
-        quote.textContent = data.content + " -" + data.author;
-    })
-    .catch(error => {
-        console.error("Error fetching quote:", error);
-    });
+        .then(data => {
+            quote.textContent =`"${data.content}" -${data.author}`
+        })
+        .catch(error => {
+            console.error("Error fetching quote:", error);
+        });
+    }
+    newQuote();
+    const newQuoteBtn = document.getElementById("newQuoteBtn");
+    newQuoteBtn.addEventListener("click", newQuote);
 });
